805d870ca9855fcd9d06a056ef6eb7da1a352d62
Vikas yadav <52526748+yadav-vikas@users.noreply.github.com>
2021-07-11 20:06:29 +0530

    queue implementation

---
+++ python-DS/queues/queue.py
@@ -0,0 +1,33 @@
+from node import Node
+
+class Queue:
+  # Add max_size and size properties within __init__():
+  def __init__(self,max_size=None):
+    self.head = None
+    self.tail = None
+    self.max_size = max_size
+    self.size = 0
+  
+  def peek(self):
+    if self.size > 0 :
+      return self.head.get_value()
+    else:
+      print("Nothing to see here!")
+  
+  # Define get_size() and has_space() below:
+  def get_size(self,max_size = None):
+    return self.size
+  def has_space(self):
+    if self.max_size:
+      if self.max_size > self.get_size():
+        return True
+      else:
+        return False
+    else:
+      return True
+  def is_empty(self):
+    if self.size == 0:
+      return True
+    else:
+      return False
+
